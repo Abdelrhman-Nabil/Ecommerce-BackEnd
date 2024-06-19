4ab6d86a0a713f9d8b14667713f76b6ffbaf719a
Abdelrhman-Nabil <aen_2012@live.com>
2024-06-19 14:46:24 +0300

    edit link

--- routes/stripe.js
+++ routes/stripe.js
@@ -65,8 +65,8 @@ router.post('/create-checkout-session', async (req, res) => {
           ],
       line_items,
       mode: 'payment',
-      success_url: `https://ecommerce-ana1.web.app/checkOut-success`,
-      cancel_url: `https://ecommerce-ana1.web.app/checkOut`,
+      success_url: `https://ecommerce-abdo1.web.app/checkOut-success`,
+      cancel_url: `https://ecommerce-abdo1.web.app/checkOut`,
     });
   
     res.send({url:session.url});
